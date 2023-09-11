c8cd9232076344f39151fa65047f29c44fb2d8cd
Justus Schock <justus.schock@posteo.de>
2023-09-11 16:35:08 +0000

    update

---
+++ tests/api/test_studio_api.py
@@ -0,0 +1,103 @@
+import pytest
+from lightning_cloud.openapi import V1CloudSpace, V1GetCloudSpaceInstanceStatusResponse
+
+from lightning_sdk.api.studio_api import StudioApi
+from lightning_sdk.machine import Machine
+
+
+def test_get_studio(internal_studio_api_mocker_get_studio):
+    studio_api = StudioApi()
+    studio = studio_api.get_studio("st-abc", "ts-abc")
+    assert isinstance(studio, V1CloudSpace)
+
+
+def test_get_studio_error(internal_studio_api_mocker_get_studio):
+    studio_api = StudioApi()
+    with pytest.raises(ValueError, match="Studio xyz does not exist"):
+        studio_api.get_studio("xyz", "ts-abc")
+
+
+@pytest.mark.parametrize("cluster", (None, "c-abc"))
+def test_create_studio(internal_studio_api_mocker_create_studio, cluster):
+    studio_api = StudioApi()
+    studio = studio_api.create_studio("st-abc", "ts-abc", cluster=cluster)
+    assert isinstance(studio, V1CloudSpace)
+    assert studio.cluster_id == cluster
+
+
+def test_get_studio_status(internal_studio_api_mocker_studio_status):
+    studio_api = StudioApi()
+    status = studio_api.get_studio_status("st-abc", "ts-abc")
+    assert isinstance(status, V1GetCloudSpaceInstanceStatusResponse)
+
+
+@pytest.mark.parametrize(
+    "machine",
+    (
+        Machine.CPU,
+        Machine.DATA_PREP,
+        Machine.T4,
+        Machine.T4_X_4,
+        Machine.V100,
+        Machine.V100_X_4,
+        Machine.A10G,
+        Machine.A10G_X_4,
+        Machine.A100_X_8,
+    ),
+)
+def test_switch_studio_machine(internal_studio_api_mocker_switch_machine, machine):
+    studio_api = StudioApi()
+    studio_api.switch_studio_machine("st-abc", "ts-abc", machine)
+
+
+def test_switch_studio_machine_wrong_machine(internal_studio_api_mocker_switch_machine):
+    studio_api = StudioApi()
+
+    with pytest.raises(KeyError, match="foo"):
+        studio_api.switch_studio_machine("st-abc", "ts-abc", "foo")
+
+
+def test_start_studio(internal_studio_api_mocker_start_studio):
+    studio_api = StudioApi()
+    studio_api.start_studio("st-abc", "ts-abc")
+
+
+def test_stop_studio(internal_studio_api_mocker_stop_studio):
+    studio_api = StudioApi()
+    studio_api.stop_studio("st-abc", "ts-abc")
+
+
+def test_run_command(internal_studio_api_mocker_run_command):
+    studio_api = StudioApi()
+
+    outputs = studio_api.run_studio_commands("st-abc", "ts-abc", "foo", "bar")
+    assert "".join(outputs) == "foo-response bar-response"
+
+
+def test_delete_studio(internal_studio_api_mocker_delete):
+    studio_api = StudioApi()
+
+    studio_api.delete_studio("st-abc", "ts-abc")
+
+
+@pytest.mark.parametrize(
+    "name,expected_machine",
+    [
+        ("st-abc", Machine.CPU),
+        ("st-def", Machine.DATA_PREP),
+        ("st-ghi", Machine.T4),
+        ("st-jkl", Machine.T4_X_4),
+        ("st-mno", Machine.V100),
+        ("st-pqr", Machine.V100_X_4),
+        ("st-stu", Machine.A10G),
+        ("st-vwx", Machine.A10G_X_4),
+        ("st-yza", Machine.A100_X_8),
+    ],
+)
+def test_get_machine(internal_studio_api_mocker_get_machine, name, expected_machine):
+    studio_api = StudioApi()
+
+    machine = studio_api.get_machine(name, "ts-abc")
+
+    assert isinstance(machine, Machine)
+    assert expected_machine == machine
---
+++ tests/test_studio.py
@@ -0,0 +1,153 @@
+from contextlib import nullcontext
+
+import pytest
+
+from lightning_sdk.machine import Machine
+from lightning_sdk.status import Status
+from lightning_sdk.studio import Studio
+
+
+@pytest.mark.parametrize("create_ok", [True, False])
+@pytest.mark.parametrize("cluster", [None, "c-abc"])
+@pytest.mark.parametrize("name", ["st-abc", "st-xyz"])
+def test_studio_init(internal_studio_init_mocker, name, cluster, create_ok):
+    # st-xyz does not exist and should not be created
+    error_out = bool(name == "st-xyz" and not create_ok)
+    if error_out:
+        contextman = pytest.raises(ValueError, match="Studio st-xyz does not exist")
+    else:
+        contextman = nullcontext()
+
+    with contextman:
+        studio = Studio(name=name, teamspace="ts-abc", org="org-abc", cluster=cluster, create_ok=create_ok)
+
+    if error_out:
+        return
+
+    assert studio.teamspace == "ts-abc"
+    assert studio.org == "org-abc"
+    assert studio.name == name
+
+
+@pytest.mark.parametrize(
+    "name,expected_status",
+    [
+        ("st-abc", Status.Pending),
+        ("st-def", Status.Pending),
+        ("st-ghi", Status.Running),
+        ("st-jkl", Status.Failed),
+        ("st-mno", Status.Stopping),
+        ("st-pqr", Status.Stopped),
+        ("st-stu", Status.Stopped),
+    ],
+)
+def test_studio_status(internal_studio_status_mocker, internal_studio_init_mocker, name, expected_status):
+    studio = Studio(name=name, teamspace="ts-abc", org="org-abc", create_ok=True)
+    assert studio.status == expected_status
+
+
+def test_studio_start(internal_studio_start_mocker, internal_studio_init_mocker):
+    studio = Studio(name="st-abc", teamspace="ts-abc", org="org-abc")
+    assert studio.status == Status.Stopped
+    assert studio.machine is None
+
+    studio.start()
+
+    assert studio.status == Status.Running
+    assert studio.machine is not None
+
+
+def test_studio_stop(internal_studio_stop_mocker, internal_studio_init_mocker):
+    studio = Studio(name="st-abc", teamspace="ts-abc", org="org-abc")
+    assert studio.status == Status.Running
+
+    studio.stop()
+
+    assert studio.status == Status.Stopped
+
+
+def test_studio_delete(internal_studio_delete_mocker):
+    studio = Studio(name="st-abc", teamspace="ts-abc", org="org-abc", create_ok=False)
+
+    studio.delete()
+
+    # doesn't exist anymore when deleted
+    with pytest.raises(ValueError, match="Studio st-abc does not exist"):
+        Studio(name="st-abc", teamspace="ts-abc", org="org-abc", create_ok=False)
+
+
+@pytest.mark.parametrize(
+    "target_machine",
+    [
+        Machine.CPU,
+        Machine.DATA_PREP,
+        Machine.T4,
+        Machine.T4_X_4,
+        Machine.V100,
+        Machine.V100_X_4,
+        Machine.A10G,
+        Machine.A10G_X_4,
+        Machine.A100_X_8,
+    ],
+)
+def test_studio_switch_machine(internal_studio_switch_mocker, internal_studio_init_mocker, target_machine):
+    studio = Studio(name="st-abc", teamspace="ts-abc", org="org-abc")
+
+    assert studio.machine is None
+    studio.start()
+
+    assert studio.machine == Machine.CPU
+    studio.switch_machine(target_machine)
+
+    assert studio.machine == target_machine
+
+
+def test_run_command(internal_studio_init_mocker, internal_studio_run_mocker):
+    studio = Studio("st-abc", "ts-abc", "org-abc")
+
+    result = studio.run("foo", "bar")
+
+    assert result == "foo-response bar-response"
+
+
+@pytest.mark.parametrize(
+    "name,expected_state,forbidden_actions",
+    [
+        ("st-def", Status.Pending, ["start", "switch", "run"]),
+        ("st-ghi", Status.Running, ["start"]),
+        ("st-jkl", Status.Failed, ["start", "stop", "switch", "run"]),
+        ("st-mno", Status.Stopping, ["start", "switch", "run", "stop"]),
+        ("st-pqr", Status.Stopped, ["stop", "switch", "run"]),
+    ],
+)
+def test_action_in_wrong_state(
+    internal_studio_init_mocker, internal_studio_status_mocker, name, expected_state, forbidden_actions
+):
+    studio = Studio(name, "ts-abc", "org-abc")
+    assert studio.status == expected_state
+
+    if "start" in forbidden_actions:
+        with pytest.raises(
+            RuntimeError, match=f"Cannot start a studio that is not stopped. Studio {name} is {expected_state}."
+        ):
+            studio.start()
+
+    if "switch" in forbidden_actions:
+        with pytest.raises(
+            RuntimeError,
+            match=f"Cannot switch machine on a studio that is not running. Studio {name} is {expected_state}.",
+        ):
+            studio.switch_machine(Machine.A10G)
+
+    if "run" in forbidden_actions:
+        with pytest.raises(
+            RuntimeError,
+            match=f"Cannot run a command in a studio that is not running. Studio {name} is {expected_state}.",
+        ):
+            studio.run("foo")
+
+    if "stop" in forbidden_actions:
+        with pytest.raises(
+            RuntimeError, match=f"Cannot stop a studio that is not running. Studio {name} is {expected_state}"
+        ):
+            studio.stop()
