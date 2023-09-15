1ef8fb526156f17bd0f1ab8ae9dbdc15ddbf4697
Justus Schock <justus.schock@posteo.de>
2023-09-15 15:11:48 +0000

    update

---
+++ lightning_sdk/plugin.py
@@ -0,0 +1,53 @@
+class Studio:
+    ...
+
+    def install(self, plugin: str):
+        ...
+
+    def run_plugin(self, name, *args, **kwargs) -> Any:
+        ...
+
+    @property
+    def all_plugins(self) -> List[Plugin]:
+        ...
+
+    def plugin(self, name: str) -> Plugin:
+        ...
+
+    def run_job(self, *commands, machine=None) -> Job:
+        ...
+
+class Plugin:
+    def run(self, *args, **kwargs):
+        ...
+
+    def id(self, *args, **kwargs):
+        ...
+
+    def studio(self, *args, **kwargs):
+        ...
+
+class JobPlugin(Plugin):
+    def run(self, *command, machine=None) -> Job:
+        ...
+
+
+class Job:
+    def __init__(
+        self,
+        name: str,
+    ) -> None:
+        self._name = name
+
+    @property
+    def status(self) -> Status:
+        ...
+
+    @property
+    def works(self) -> List[Work]:
+        ...
+    
+    @property
+    def stop(self):
+        # stop all works and orchestrator
+        ...
