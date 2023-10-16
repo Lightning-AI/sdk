0d9639b81a6be0581fbb06bf26d8369a804ffa46
Justus Schock <justus.schock@posteo.de>
2023-10-16 15:08:38 +0000

    update

--- lightning_sdk/plugin.py
+++ lightning_sdk/plugin.py
@@ -1,9 +1,10 @@
-from typing import Any, List, Optional, Protocol, runtime_checkable, TYPE_CHECKING
+import datetime
 import logging
 from abc import ABC, abstractmethod
-from lightning_sdk.studio import Studio
-import datetime
+from typing import TYPE_CHECKING, Any, Optional, Protocol, runtime_checkable
+
 from lightning_sdk.machine import Machine
+from lightning_sdk.studio import Studio
 from lightning_sdk.utils import _setup_logger
 
 if TYPE_CHECKING:
@@ -13,46 +14,63 @@ _logger = _setup_logger(__name__)
 
 
 class _Plugin(ABC):
+    """Abstract Plugin class defining the API.
+
+    Args:
+        name: the name of the current plugin
+        description: the description of the current plugin
+        studio: the Studio, the current plugin is installed on
+
+    """
+
     def __init__(
         self,
         name: str,
         description: str,
         studio: Studio,
-    ):
+    ) -> None:
         self._name = name
         self._description = description
         self._studio = studio
         self._has_been_executed = False
 
-    def install(self):
+    def install(self) -> None:
+        """Installs the plugin on the Studio given at init-time."""
         self._studio._install_plugin(self._name)
 
-    def uninstall(self):
+    def uninstall(self) -> None:
+        """Uninstalls the plugin from the Studio given at init-time."""
         self._studio._uninstall_plugin(self._name)
 
     @abstractmethod
-    def run(self, *args, **kwargs) -> Any:
-        pass
+    def run(self, *args: Any, **kwargs: Any) -> Any:
+        """Runs the plugin on the Studio given at init-time."""
 
     @property
     def name(self) -> str:
+        """The name of the current plugin."""
         return self._name
 
     @property
     def description(self) -> str:
+        """The description of the current plugin."""
         return self._description
 
     @property
     def studio(self) -> str:
+        """The Studio, the current plugin is installed on."""
         return self._studio.name
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """String representation of the current plugin."""
         return f"Plugin(\n\tname={self.name}\n\tdescription={self.description}\n\tstudio={self.studio})"
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """String representation of the current plugin."""
         return repr(self)
 
-    def __eq__(self, other):
+    def __eq__(self, other: "Plugin") -> bool:
+        """Checks for equality with other plugins."""
         return (
             type(self) is type(other)
             and self.name == other.name
@@ -62,10 +80,13 @@ class _Plugin(ABC):
 
 
 class Plugin(_Plugin):
-    def run(self):
+    """Plugin class to handle arbitrary plugins on the Studio."""
+
+    def run(self) -> str:
+        """Executes the command of the plugin on the Studio."""
         if self._has_been_executed:
             logging.info("This plugin has already been executed and can be run only once per Studio.")
-            return
+            return None
 
         output, port = self._studio._execute_plugin(self._name)
 
@@ -76,6 +97,8 @@ class Plugin(_Plugin):
 
 
 class JobsPlugin(_Plugin):
+    """Plugin handling asynchronous jobs."""
+
     _plugin_run_name = "Job"
     _slug_name = "jobs"
 
@@ -84,7 +107,8 @@ class JobsPlugin(_Plugin):
         command: str,
         name: Optional[str] = None,
         cloud_compute: Machine = Machine.CPU,
-    ):
+    ) -> "Externalv1LightningappInstance":
+        """Launches an asynchronous job."""
         if name is None:
             name = _run_name("job")
         resp = self._studio._studio_api.create_job(
@@ -101,6 +125,8 @@ class JobsPlugin(_Plugin):
 
 
 class MultiMachineTrainingPlugin(_Plugin):
+    """Plugin handling multi-machine-training jobs."""
+
     _plugin_run_name = "Multi-Machine-Training"
     _slug_name = "mmt"
 
@@ -111,7 +137,8 @@ class MultiMachineTrainingPlugin(_Plugin):
         cloud_compute: Machine = Machine.CPU,
         num_instances: int = 2,
         strategy: str = "parallel",
-    ):
+    ) -> "Externalv1LightningappInstance":
+        """Launches an asynchronous multi-machine-training."""
         if name is None:
             name = _run_name("dist-run")
 
@@ -132,6 +159,8 @@ class MultiMachineTrainingPlugin(_Plugin):
 
 
 class InferenceServerPlugin(_Plugin):
+    """Plugin handling the asynchronous inference server."""
+
     _plugin_run_name = "Inference Server"
     _slug_name = ""
 
@@ -147,7 +176,8 @@ class InferenceServerPlugin(_Plugin):
         max_batch_size: int = 4,
         timeout_batching: float = 0.3,
         endpoint: str = "/predict",
-    ):
+    ) -> "Externalv1LightningappInstance":
+        """Launches an asynchronous inference server."""
         if name is None:
             name = _run_name("inference-run")
 
@@ -176,13 +206,17 @@ class _RunnablePlugin(Protocol):
     _plugin_run_name: str
     _slug_name: str
 
-    def run(self, command: str, name: Optional[str] = None, cloud_compute: Machine = Machine.CPU, **kwargs):
+    def run(
+        self, command: str, name: Optional[str] = None, cloud_compute: Machine = Machine.CPU, **kwargs: Any
+    ) -> "Externalv1LightningappInstance":
         ...
 
 
-def _run_name(plugin_type: str):
+def _run_name(plugin_type: str) -> str:
+    """Creates the run name for a given plugin type."""
     return f"{plugin_type}-{datetime.datetime.now().strftime('%b-%d-%H_%M')}"
 
 
-def _success_message(resp: "Externalv1LightningappInstance", plugin_instance: _RunnablePlugin):
+def _success_message(resp: "Externalv1LightningappInstance", plugin_instance: _RunnablePlugin) -> str:
+    """Compiles the success message for a given runnable plugin."""
     return f"{plugin_instance._plugin_run_name} {resp.name} was successfully launched. View it at https://lightning.ai/{plugin_instance._studio.owner}/{plugin_instance._studio._teamspace.name}/studios/{plugin_instance.studio}/app?app_id={plugin_instance._slug_name}&job_name={resp.name}"
