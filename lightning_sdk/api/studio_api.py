af146e8c54ab310d6018aeee8be7624fc4924dcd
Justus Schock <justus.schock@posteo.de>
2023-10-17 19:45:22 +0000

    update

--- lightning_sdk/api/studio_api.py
+++ lightning_sdk/api/studio_api.py
@@ -132,15 +132,18 @@ class StudioApi:
         )
 
         # block until studio is really stopped
-        while self._get_studio_instance_status(studio_id=studio_id, teamspace_id=teamspace_id) not in (None, "CLOUD_SPACE_INSTANCE_STATE_STOPPED"):
+        while self._get_studio_instance_status(studio_id=studio_id, teamspace_id=teamspace_id) not in (
+            None,
+            "CLOUD_SPACE_INSTANCE_STATE_STOPPED",
+        ):
             time.sleep(1)
 
     def _get_studio_instance_status(self, studio_id: str, teamspace_id: str) -> Optional[str]:
-        """Returns status of the in-use instance of the Studio"""
+        """Returns status of the in-use instance of the Studio."""
         internal_status = self.get_studio_status(studio_id=studio_id, teamspace_id=teamspace_id).in_use
         if internal_status is None:
             return None
-        
+
         return internal_status.phase
 
     def switch_studio_machine(self, studio_id: str, teamspace_id: str, machine: Machine) -> None:
@@ -506,18 +509,12 @@ def _upload_file_to_urls(*urls: Union[str, V1MultiPartPresignedUrl], path: str, 
         reader_wrapper = CallbackIOWrapper(update_fn, fd, "read")
 
         for url in urls:
-            if isinstance(url, V1MultiPartPresignedUrl):
-                curr_url = url.url
-            else:
-                curr_url = url
+            curr_url = url.url if isinstance(url, V1MultiPartPresignedUrl) else url
 
-            if len(urls) > 1:
-                # unfortunately we can't just pass the reader_wrapper directly since we only
-                # need to read the first N bytes, finding a way to still pass the reader_wrapper
-                # would likely be faster though
-                data = reader_wrapper.read(_MAX_SIZE_MULTI_PART_CHUNK)
-            else:
-                data = reader_wrapper
+            # unfortunately we can't just pass the reader_wrapper directly since we only
+            # need to read the first N bytes, finding a way to still pass the reader_wrapper
+            # would likely be faster though
+            data = reader_wrapper.read(_MAX_SIZE_MULTI_PART_CHUNK) if len(urls) > 1 else reader_wrapper
 
             response = requests.put(curr_url, data=data)
             response.raise_for_status()
