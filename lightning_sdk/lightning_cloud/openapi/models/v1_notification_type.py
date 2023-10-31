bb8bd664ec1ede6db7f46ccb210dfef0d96acd37
andyland <andyland@users.noreply.github.com>
2023-10-31 11:56:52 +0000

    Vendor Cloud Changes

--- lightning_sdk/lightning_cloud/openapi/models/v1_notification_type.py
+++ lightning_sdk/lightning_cloud/openapi/models/v1_notification_type.py
@@ -39,6 +39,7 @@ class V1NotificationType(object):
     """
     UNSPECIFIED = "NOTIFICATION_TYPE_UNSPECIFIED"
     LOW_FUNDS = "NOTIFICATION_TYPE_LOW_FUNDS"
+    LONG_WORKLOADS = "NOTIFICATION_TYPE_LONG_WORKLOADS"
     """
     Attributes:
       swagger_types (dict): The key is attribute name
--- lightning_sdk/lightning_cloud/resolver.py
+++ lightning_sdk/lightning_cloud/resolver.py
@@ -23,6 +23,9 @@ class _LightningSrcResolver(_Resolver):
     """The `_LightningSrcResolver` enables to retrieve a cloud storage path from a directory."""
 
     def __call__(self, root: str) -> Optional[str]:
+        if root.startswith("s3://"):
+            return root
+
         root_absolute = str(Path(root).absolute())
 
         if root_absolute.startswith("/teamspace/studios/this_studio"):
@@ -36,6 +39,9 @@ class _LightningSrcResolver(_Resolver):
 
         if root_absolute.startswith("/teamspace/s3_connections") and len(root_absolute.split("/")) > 3:
             return self._resolve_s3_connections(root_absolute)
+
+        if root_absolute.startswith("/teamspace/datasets") and len(root_absolute.split("/")) > 3:
+            return self._resolve_datasets(root_absolute)
 
         return None
 
@@ -98,6 +104,49 @@ class _LightningSrcResolver(_Resolver):
 
         return os.path.join(data_connection[0].aws.source, *root.split("/")[4:])
 
+    def _resolve_datasets(self, root: str) -> str:
+        client = LightningClient()
+
+        # Get the ids from env variables
+        cluster_id = os.getenv("LIGHTNING_CLUSTER_ID", None)
+        project_id = os.getenv("LIGHTNING_CLOUD_PROJECT_ID", None)
+        cloud_space_id = os.getenv("LIGHTNING_CLOUD_SPACE_ID", None)
+
+        if cluster_id is None:
+            raise RuntimeError("The `cluster_id` couldn't be found from the environement variables.")
+
+        if project_id is None:
+            raise RuntimeError("The `project_id` couldn't be found from the environement variables.")
+
+        if cloud_space_id is None:
+            raise RuntimeError("The `cloud_space_id` couldn't be found from the environement variables.")
+
+        clusters = client.cluster_service_list_clusters().clusters
+
+        target_cloud_space = [
+            cloudspace
+            for cloudspace in client.cloud_space_service_list_cloud_spaces(
+                project_id=project_id, cluster_id=cluster_id
+            ).cloudspaces
+            if cloudspace.id == cloud_space_id
+        ]
+
+        if not target_cloud_space:
+            raise ValueError(f"We didn't find any matching Studio for the provided id `{cloud_space_id}`.")
+
+        target_cluster = [cluster for cluster in clusters if cluster.id == target_cloud_space[0].cluster_id]
+
+        if not target_cluster:
+            raise ValueError(
+                f"We didn't find a matching cluster associated with the id {target_cloud_space[0].cluster_id}."
+            )
+
+        bucket_name = target_cluster[0].spec.aws_v1.bucket_name
+
+        return os.path.join(
+            f"s3://{bucket_name}/projects/{project_id}/datasets/",
+            *root.split("/")[3:],
+        )
 
 class _LightningTargetResolver(_Resolver):
     """The `_LightningTargetResolver` generates a cloud storage path from a directory."""
