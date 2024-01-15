819c46e6424dba4810e2b1662e5f9370659caf76
Justus Schock <12886177+justusschock@users.noreply.github.com>
2024-01-15 16:35:14 +0100

    version_bump

--- lightning_sdk/__init__.py
+++ lightning_sdk/__init__.py
@@ -3,6 +3,13 @@ from lightning_sdk.plugin import JobsPlugin, MultiMachineTrainingPlugin, Plugin
 from lightning_sdk.status import Status
 from lightning_sdk.studio import Studio
 
-__all__ = ["JobsPlugin", "Machine", "MultiMachineTrainingPlugin", "Plugin", "Status", "Studio"]
+__all__ = [
+    "JobsPlugin", 
+    "Machine", 
+    "MultiMachineTrainingPlugin", 
+    "Plugin", 
+    "Status", 
+    "Studio",
+]
 
-__version__ = "0.0.14alpha"
+__version__ = "0.0.15alpha"
