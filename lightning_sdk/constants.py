df5e2d977f0d64b892119b8a035356b8ffa58155
thomas chaton <thomas@grid.ai>
2024-05-29 16:29:33 +0100

    Add global lightning run id (#269)

--- lightning_sdk/constants.py
+++ lightning_sdk/constants.py
@@ -1,4 +1,5 @@
 import os
+from uuid import uuid4
 
 _LIGHTNING_DEBUG = {
     "": False,
@@ -9,3 +10,6 @@ _LIGHTNING_DEBUG = {
     "true": True,
     "yes": True,
 }.get(os.getenv("LIGHTNING_DEBUG", "").lower(), False)
+
+# The LightningRunId is created globally to be available across all processes and threads
+__GLOBAL_LIGHTNING_RUN_ID__ = str(uuid4().hex)
