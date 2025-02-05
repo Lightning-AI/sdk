2f0cfac7c2b7916b3b11e1385b4cf8992a6c4e97
Aniket Maurya <theaniketmaurya@gmail.com>
2025-02-05 22:11:32 +0000

    fix automatic LCR login on container upload and download (#608)
    * fix
    
    * fix
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- lightning_sdk/api/lit_container_api.py
+++ lightning_sdk/api/lit_container_api.py
@@ -1,6 +1,7 @@
-from typing import Generator, List
+from typing import Any, Callable, Generator, List
 
 import docker
+import requests
 
 from lightning_sdk.api.utils import _get_registry_url
 from lightning_sdk.lightning_cloud.env import LIGHTNING_CLOUD_URL
@@ -11,7 +12,22 @@ from lightning_sdk.teamspace import Teamspace
 
 class LCRAuthFailedError(Exception):
     def __init__(self) -> None:
-        super().__init__("Failed to authenticate with Lightning Container Registry")
+        super().__init__(
+            "Failed to authenticate with Lightning Container Registry. Please login manually "
+            "using the following command:\n  "
+            "echo $LIGHTNING_API_KEY | docker login litcr.io --username=LIGHTNING_USERNAME --password-stdin"
+        )
+
+
+def retry_on_lcr_auth_failure(func: Callable) -> Callable:
+    def wrapper(self: "LitContainerApi", *args: Any, **kwargs: Any) -> Callable:
+        try:
+            return func(self, *args, **kwargs)
+        except LCRAuthFailedError:
+            self.authenticate()
+            return func(self, *args, **kwargs)
+
+    return wrapper
 
 
 class LitContainerApi:
@@ -38,10 +54,10 @@ class LitContainerApi:
     def delete_container(self, project_id: str, container: str) -> V1DeleteLitRepositoryResponse:
         try:
             return self._client.lit_registry_service_delete_lit_repository(project_id, container)
-        except Exception as ex:
-            raise ValueError(f"Could not delete container {container} from project {project_id}") from ex
+        except Exception as e:
+            raise ValueError(f"Could not delete container {container} from project {project_id}: {e!s}") from e
 
-    def upload_container(self, container: str, teamspace: Teamspace, tag: str) -> Generator[str, None, None]:
+    def upload_container(self, container: str, teamspace: Teamspace, tag: str) -> Generator[dict, None, None]:
         try:
             self._docker_client.images.get(container)
         except docker.errors.ImageNotFound:
@@ -55,19 +71,23 @@ class LitContainerApi:
             raise ValueError(f"Could not tag container {container} with {repository}:{tag}")
         lines = self._docker_client.api.push(repository, stream=True, decode=True)
         for line in lines:
-            if "errorDetail" in line and "authorization failed" in line["error"]:
+            if "errorDetail" in line and ("authorization failed" in line["error"] or "unauth" in line["error"]):
                 raise LCRAuthFailedError()
             yield line
         yield {
             "finish": True,
-            "url": f"{LIGHTNING_CLOUD_URL}/{teamspace.owner.name}/{teamspace.name}/containers/{container}",
+            "url": f"{LIGHTNING_CLOUD_URL}/{teamspace.owner.name}/{teamspace.name}/containers/{container_basename}",
         }
 
+    @retry_on_lcr_auth_failure
     def download_container(self, container: str, teamspace: Teamspace, tag: str) -> Generator[str, None, None]:
         registry_url = _get_registry_url()
         repository = f"{registry_url}/lit-container/{teamspace.owner.name}/{teamspace.name}/{container}"
         try:
             self._docker_client.images.pull(repository, tag=tag)
+        except requests.exceptions.HTTPError as e:
+            if "unauthorized" in e.response.text:
+                raise LCRAuthFailedError() from e
         except docker.errors.APIError as e:
             raise ValueError(f"Could not pull container {container} from {repository}:{tag}") from e
         return self._docker_client.api.tag(repository, container, tag)
