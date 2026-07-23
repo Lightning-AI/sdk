94d01ec700007ba590b49fb0133474e32a0805fe
Chito Kim <nekroztrap@gmail.com>
2026-07-24 04:33:28 +0900

    code formatting

--- python/lightning_sdk/cli/utils/terminal_menu_wrapper.py
+++ python/lightning_sdk/cli/utils/terminal_menu_wrapper.py
@@ -1,27 +1,30 @@
 import os
-from typing import List, Optional
+from typing import Any, List, Optional
 
 if os.name == "nt":
-    import inquirer # import only for windows
+    import inquirer  # import only for windows
+
     class TerminalMenu:
-        def __init__(self, options: List[str], title: Optional[str] = None, **kwargs):
+        def __init__(
+            self,
+            options: List[str],
+            title: Optional[str] = None,
+            **kwargs: Any,
+        ) -> None:
             self.options = options
             self.title = title or "Please select an option:"
             self.chosen_menu_index: Optional[int] = None
-        def show(self):
+
+        def show(self) -> None:
             # generate inquirer questons
-            questions = [
-                inquirer.List('choice',
-                            message=self.title,
-                            choices=self.options)
-            ]
+            questions = [inquirer.List("choice", message=self.title, choices=self.options)]
             answer = inquirer.prompt(questions)
-            
+
             # Keyboard Interrupt
-            if not answer or 'choice' not in answer:
+            if not answer or "choice" not in answer:
                 self.chosen_menu_index = None
                 return
-            
-            self.chosen_menu_index = self.options.index(answer['choice'])
+
+            self.chosen_menu_index = self.options.index(answer["choice"])
 else:
-    from simple_term_menu import TerminalMenu
+    from simple_term_menu import TerminalMenu  # noqa: F401
