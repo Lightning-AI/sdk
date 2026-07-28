d781191c8acc6daf252e48a2548ef2116bf91441
Ethan Harris <ethanwharris@gmail.com>
2026-07-28 09:21:50 +0000

    Feature: Auth introspection - whoami

---
+++ python/lightning_sdk/cli/auth/__init__.py
@@ -0,0 +1,10 @@
+"""Auth CLI commands."""
+
+import rich_click as click
+
+
+def register_commands(group: click.Group) -> None:
+    """Register auth commands with the given group."""
+    from lightning_sdk.cli.auth.whoami import whoami
+
+    group.add_command(whoami)
